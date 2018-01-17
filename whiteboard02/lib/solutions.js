32bd75c032c48e23f384d50fad6ddc14ab526d54
Victoria Oh <veoh1989@gmail.com>
2018-01-16 16:38:27 -0800

    all tests passing

--- whiteboard02/lib/solutions.js
+++ whiteboard02/lib/solutions.js
@@ -1,15 +1,13 @@
 'use strict';
 
-var arr = [23, 23, 4, 4, 4,4, 4, 55, 62, 9, 12, 36, 97, 87, 65];
-
-function solution() {
+exports.final = function finalNum(arr){
+  if (!arr || !Array.isArray(arr) || arr.length < 2) return null;
+  for (let i in arr) if (typeof arr[i] !== 'number') return null;
   let unique_array = Array.from(new Set(arr)).sort(function sortNumber(a,b) {
     return a - b;
   });
   return {
     max: Math.max(...unique_array),
-    runnerUp: unique_array[unique_array.length - 2]
+    runnerUp: unique_array[unique_array.length -2]
   };
-}
-
-solution();
+};
